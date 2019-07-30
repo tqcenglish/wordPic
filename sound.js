6f2b2be10eb0b8caded45348602daa91ef24c7eb
tqcenglish <tqcenglish@gmail.com>
2019-07-31 01:45:26 +0800

    fix

--- sound.js
+++ sound.js
@@ -12,31 +12,32 @@ const baseUrl = `http://www.yingyudanci.com/voicefile`;
 
 let chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-for (let index = 0; index < chars.length; index++) {
-    const char = chars[index];
-    let root = path.join(__dirname, 'out', char);
-    console.log(root);
-    if( fs.existsSync(root)){
-        let words = fs.readdirSync(root);
-        for (let index = 0; index < words.length; index++) {
-            const word = words[index];
-            if (word.startsWith('.')) {
-                continue;
+(async () => {
+
+    for (let index = 0; index < chars.length; index++) {
+        const char = chars[index];
+        let root = path.join(__dirname, 'out', char);
+        console.log(root);
+        if( fs.existsSync(root)){
+            let words = fs.readdirSync(root);
+            for (let index = 0; index < words.length; index++) {
+                const word = words[index];
+                if (word.startsWith('.')) {
+                    continue;
+                }
+                let url = `${baseUrl}/${word}.mp3`;
+                console.log(`download ${url}`);
+                try {
+                    await pipeline(
+                        got.stream(url),
+                        fs.createWriteStream(`${root}/${word}/${word}.mp3`)
+                    );  
+                } catch (error) {
+                    console.error(`download ${word} failure`);
+                }
+ 
             }
-            let url = `${baseUrl}/${word}.mp3`;
-            console.log(url);
-    
-            pipeline(
-                got.stream(url),
-                fs.createWriteStream(`${root}/${word}/${word}.mp3`),
-                (err => {
-                    if (err) {
-                        console.error('Pipeline failed.', err);
-                    } else {
-                        console.log('Pipeline succeeded.');
-                    }
-                })
-            );   
         }
     }
-}
+})()
+
